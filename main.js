bb4ad58c204e19e3b4abed28e930260ddf7fd608
Moreli Binyamini Harel <Moreligirl.S@gmail.com>
2022-03-17 14:28:03 +0200

    Add files via upload
    fixed some stuff, cleaned up javascript

--- main.js
+++ main.js
@@ -1,116 +1,119 @@
-const taskList = [];
-
-const exampleTask = {
-  description: "",
-  due: new Date(Date.now())
-};
-
-
+/* Basic Functions */
 
 function getTaskFromUserInput() {
   const taskDescription = document.getElementById("taskDescription").value;
   const dueDate = document.getElementById("dueDate").value;
   const dueTime = document.getElementById("dueTime").value;
 
-  const timestamp = new Date(dueDate + "T" + dueTime+"Z");
+  const timestamp = new Date(`${dueDate}T${dueTime}`);
 
   const task = {
     description: taskDescription,
-    dueDate: getDueDate(timestamp),
-    dueTime: getDueTime(timestamp)
+    dueDate: getFormattedDueDate(timestamp),
+    dueTime: getFormattedDueTime(timestamp)
   };
-
-  // console.log(task); // not needed
   return task;
-
 }
 
 function addTask() {
-
+  // reset invalid status before validating so that corrected fields will not be marked invalid
   clearInputValidityStatus();
-  // validate
+
   const userInputIsValid = validateUserInput();
-  // if invalid, dont continue
+  // if invalid input, dont continue
   if (!userInputIsValid) {
     return;
   }
-
+  // if we got here, the input is valid
   const task = getTaskFromUserInput();
+
+  addTaskToLocalStorage(task);
+  addTaskToDOM(task);
 
   // reset form
   clearUserInput();
-
-  addTaskToLocalStorage(task);
-
-  // add task to DOM
-  addTaskToDOM(task);
 }
 
-function addTaskToDOM(task) {
+function removeTask(event) {
+  // find which task was called
+  const index = getElementIndexOfSelectedNote(event);
 
+  // remove that task
+  removeTaskFromLocalStorage(index);
+  removeTaskFromDOM(index);
+}
+
+
+
+// Functions responsible for DOM and visual display
+
+function addTaskToDOM(task) {
   // the container for placing all the task notes inside
   const taskContainer = document.getElementById("taskContainer");
 
   // create a task note element
-  const note = document.createElement("div");
-  note.classList.add("col-auto", "m-3", "note", "position-relative", "fade-in"); // add classes
+  const taskNote = document.createElement("div");
+  taskNote.classList.add("col-auto", "m-3", "note", "position-relative", "fade-in"); // add classes
 
   // create the remove button
   const removeTaskButton = document.createElement("button");
   removeTaskButton.classList.add("position-absolute", "remove-button", "btn", "btn-dark"); // add classes
-  removeTaskButton.onclick = removeTask; // onclick
+  removeTaskButton.onclick = removeTask; // add button action
   removeTaskButton.innerHTML = `<i class="bi bi-x-lg"></i>`; // icon
   // add to the note element
-  note.appendChild(removeTaskButton);
-
+  taskNote.appendChild(removeTaskButton);
 
   // create the text section for the task description
   const taskDescriptionText = document.createElement("div");
   taskDescriptionText.classList.add("note-description", "p-0", "overflow-auto"); // classes
-  taskDescriptionText.innerText = task.description; // text
+  taskDescriptionText.innerText = task.description; // add the task description text
   // add to the note element
-  note.appendChild(taskDescriptionText);
-
+  taskNote.appendChild(taskDescriptionText);
 
   // create the text section for the date
   const taskDueDateText = document.createElement("div");
   taskDueDateText.classList.add("p-0", "note-timestamp"); // classes
   taskDueDateText.innerText = task.dueDate; // add the task's due date inside the date element
   // add to note
-  note.appendChild(taskDueDateText);
-
+  taskNote.appendChild(taskDueDateText);
 
   // create the text section for the time
   const taskDueTimeText = document.createElement("div");
-  taskDueTimeText.classList.add("p-0", "note-timestamp"); //
+  taskDueTimeText.classList.add("p-0", "note-timestamp");
   taskDueTimeText.innerText = task.dueTime; // add the task's due time inside the time element
   // add to note
-  note.appendChild(taskDueTimeText);
+  taskNote.appendChild(taskDueTimeText);
 
-
-  // add note to container
-  taskContainer.appendChild(note);
-
+  // add finished note to container
+  taskContainer.appendChild(taskNote);
 }
 
-function getTaskListFromLocalStorage() {
-
-  // get JSON from local storage
-  let taskListJSON = localStorage.getItem("taskList");
-  // turn JSON string to array object
-  let taskList = JSON.parse(taskListJSON);
-
-  // if the user hasn't logged any tasks yet, the local storage would be empty
-  if (taskList === null) {
-    // if taskList doesn't exist, use an empty array to initialise it
-    taskList = [];
-  }
-
-  return taskList;
-
+function removeTaskFromDOM(index) {
+  // find the note and remove it
+  document.getElementById("taskContainer").children[index].remove();
 }
 
-// updates the value of taskList in local storage
+function getElementIndexOfSelectedNote(event) {
+  // the current target is the remove button, the selected note would be its parent
+  const selectedNote = event.currentTarget.parentElement;
+
+  const noteList = document.getElementById("taskContainer").children;
+  // find our note in the list of all notes
+  const index = Array.from(noteList).indexOf(selectedNote);
+
+  return index;
+}
+
+function displayAllTasks() {
+  const taskList = getTaskListFromLocalStorage();
+  // go over the list and display every note
+  taskList.forEach(addTaskToDOM);
+}
+
+
+
+/* functions responsible for local storage */
+
 function updateTaskListInLocalStorage(taskList) {
 
   // turn array to JSON
@@ -121,176 +124,154 @@ function updateTaskListInLocalStorage(taskList) {
 
 }
 
-// adds a task and updates local storage
 function addTaskToLocalStorage(task) {
 
-  // get taskList
   let taskList = getTaskListFromLocalStorage();
 
   // add task to array
   taskList.push(task);
 
-  // update
+  // update new array
   updateTaskListInLocalStorage(taskList);
-
 }
 
-// removes a task and updates local storage
 function removeTaskFromLocalStorage(index) {
 
-  // get taskList
   let taskList = getTaskListFromLocalStorage();
 
   // remove task from array
   taskList.splice(index, 1);
 
-  // update
+  // update new array
   updateTaskListInLocalStorage(taskList);
 }
 
-function removeTask(event) {
+function getTaskListFromLocalStorage() {
 
-  // find which task was called
-  const index = getElementIndexOfSelectedNote(event);
+  const taskListJSON = localStorage.getItem("taskList");
+  // if the user hadn't logged any tasks yet, the local storage would be empty
 
-  // remove from local storage
-  removeTaskFromLocalStorage(index);
-
-  // remove from DOM
-  removeTaskFromDOM(index);
-
-}
-
-function removeTaskFromDOM(index) {
-
-  document.getElementById("taskContainer").children[index].remove();
-}
-
-function getElementIndexOfSelectedNote(event) {
-
-  const selectedNote = event.currentTarget.parentElement;
-
-  const noteList = document.getElementById("taskContainer").children;
-
-  const index = Array.from(noteList).indexOf(selectedNote);
-
-  return index;
-}
-
-function clearUserInput() {
-  //
-
-  document.getElementById("taskDescription").value = "";
-  document.getElementById("dueDate").value = "";
-  document.getElementById("dueTime").value = "";
-
-  clearInputValidityStatus();
-
-  document.getElementById("taskDescription").focus();
-}
-
-function clearInputValidityStatus() {
-
-  //
-  document.getElementById("taskDescription").classList.remove("is-invalid");
-  document.getElementById("dueDate").classList.remove("is-invalid");
-  document.getElementById("dueTime").classList.remove("is-invalid");
-
-}
-
-function validateUserInput(task) {
-  //
-
-  const taskDescription = document.getElementById("taskDescription").value;
-  const dueTime = document.getElementById("dueTime").value;
-  const dueDate = document.getElementById("dueDate").value;
-
-  let userInputIsValid = true;
-
-  if (taskDescription === "") {
-
-    // description is empty
-    userInputIsValid = false;
-    // display error
-    document.getElementById("taskDescription").classList.add("is-invalid");
-
-  }
-
-  if (dueTime === "") {
-
-    // time empty
-    userInputIsValid = false;
-    // display error
-    document.getElementById("dueTimeFeedback").innerText = "Please provide a due time.";
-    document.getElementById("dueTime").classList.add("is-invalid");
-
-  }
-
-  if (dueDate === "") {
-
-    // date empty
-    userInputIsValid = false;
-    // display error
-    document.getElementById("dueDateFeedback").innerText = "Please provide a due date.";
-    document.getElementById("dueDate").classList.add("is-invalid");
+  if (taskListJSON === null) {
+    // since the user hadn't logged any tasks, the list should be empty
+    return [];
 
   } else {
-
-    // is now >= due date+time?
-    // if true: date overdue
-    // is now date only == due date only?
-    // is true: time overdue too
-
-    const currentTimestamp = new Date();
-    const dueTimestamp = new Date(`${dueDate}T${dueTime}Z`);
-
-
-    if (currentTimestamp.getTime() > dueTimestamp.getTime()) {
-
-
-      // for sure overdue
-      userInputIsValid = false;
-
-
-      // check for time
-      // we will alter the timestamps by setting them to midnight
-      currentTimestamp.setHours(0, 0, 0, 0);
-      dueTimestamp.setHours(0, 0, 0, 0);
-
-      if (currentTimestamp.getTime() === dueTimestamp.getTime()) { // if today
-        //
-        if (dueTime !== "") {
-          // time invalid
-
-          document.getElementById("dueTimeFeedback").innerText = "This time has passed already!";
-          document.getElementById("dueTime").classList.add("is-invalid");
-          // date valid
-
-        }
-        // else - already invalid bc of empty time
-
-      } else { // not today
-
-        // date invalid
-        document.getElementById("dueDateFeedback").innerText = "This date has passed already!";
-        document.getElementById("dueDate").classList.add("is-invalid");
-
-      }
-
-    }
-
+    // turn JSON string to array object and return it
+    const taskList = JSON.parse(taskListJSON);
+    return taskList;
   }
+}
 
-  // at this point, if there was an invalid input, userInputIsValid would be set to false
+
+
+// form validation
+
+function validateUserInput() {
+  // we're going to assume the input is valid, and try to disprove it
+  let userInputIsValid = true;
+
+  // check each field if it's empty or not; display an appropriate message if it is
+  const taskDescriptionIsEmpty = checkIfInputFieldIsEmpty("taskDescription");
+  const dueTimeIsEmpty = checkIfInputFieldIsEmpty("dueTime");
+  const dueDateIsEmpty = checkIfInputFieldIsEmpty("dueDate");
+
+  // if any of the fields are empty, the user input is invalid
+  if (taskDescriptionIsEmpty || dueTimeIsEmpty || dueDateIsEmpty) {
+    userInputIsValid = false;
+  }
+  // since we have a date, we can do an additional check for if it has passed already
+  if (!dueDateIsEmpty) {
+    const userInputIsOverDue = checkIfUserInputIsOverdue();
+
+    if (userInputIsOverDue) {
+      userInputIsValid = false;
+    }
+  }
+  // at this point, if there was an invalid input, userInputIsValid would have been set to false.
   return userInputIsValid;
 }
 
+function checkIfInputFieldIsEmpty(inputFieldId) {
+
+  const inputField = document.getElementById(inputFieldId);
+
+  if (inputField.value === "") {
+    // if field is empty, display appropriate message
+    displayFeedbackForInvalidUserInput(inputFieldId, `Please provide a ${inputField.ariaLabel}.`);
+    return true;
+  }
+  // if we got here, field is not empty
+  return false;
+}
+
+function checkIfUserInputIsOverdue() {
+  // first of all we need to get the values
+  const dueDate = document.getElementById("dueDate").value; // is definitely NOT empty
+  const dueTime = document.getElementById("dueTime").value; // may or may not be empty
+
+  // if dueTime is empty, replace it with 00:00 so that we can compare just the date
+  const dueDateTimeAsISOString = `${dueDate}T${ dueTime ? dueTime : "00:00" }`;
+  // create date objects for easy comparison
+  const currentTimestamp = new Date();
+  const dueTimestamp = new Date(dueDateTimeAsISOString);
+
+  if (currentTimestamp.getTime() > dueTimestamp.getTime()) {
+    // either dueDate or dueTime are for sure overdue, check which one
+
+    // set time to midnight so that we can compare just the dates
+    currentTimestamp.setHours(0, 0, 0, 0);
+    dueTimestamp.setHours(0, 0, 0, 0);
+    // check if the inputted date is the current date
+    if (currentTimestamp.getTime() === dueTimestamp.getTime()) {
+      // due date is today, therefore it is valid; which means either the due time is overdue, or the field was empty.
+      if (dueTime !== "") {
+        // if the due time is not empty, it's definitely overdue.
+        displayFeedbackForInvalidUserInput("dueTime", "This time has passed already!");
+      }
+    } else {
+      // if the due date is not today, it has to be a past date which means it is overdue.
+      displayFeedbackForInvalidUserInput("dueDate", "This date has passed already!");
+    }
+
+    // whether it's the time or date, the user input is definitely overdue.
+    return true;
+  }
+  // if we got here, that means the user input is not overdue
+  return false;
+}
+
+function displayFeedbackForInvalidUserInput(inputField, message) {
+  // update correct message
+  document.getElementById(`${inputField}Feedback`).innerText = message;
+  // mark field invalid to display message
+  document.getElementById(inputField).classList.add("is-invalid");
+}
 
 
-/* DATE AND TIME FORMATTING FUNCTIONS */
 
-// gets a Date object as a parameter and returns a formatted date string
-function getDueDate(timestamp) {
+// functions responsible for controlling inputs
+
+function clearUserInput() {
+  // clear the values
+  document.getElementById("taskDescription").value = "";
+  document.getElementById("dueDate").value = "";
+  document.getElementById("dueTime").value = "";
+  // clear the invalid appearance
+  clearInputValidityStatus();
+}
+
+function clearInputValidityStatus() {
+  // the input is marked invalid by adding the is-invalid class, so just remove it
+  document.getElementById("taskDescription").classList.remove("is-invalid");
+  document.getElementById("dueDate").classList.remove("is-invalid");
+  document.getElementById("dueTime").classList.remove("is-invalid");
+}
+
+
+
+// utility stuff
+
+function getFormattedDueDate(timestamp) {
 
   // formatting options for date to show leading zeroes, in a shortened format
   const dateFormattingOptions = {
@@ -304,8 +285,7 @@ function getDueDate(timestamp) {
   return dueDate;
 }
 
-// gets a Date object as a parameter and returns a formatted time string
-function getDueTime(timestamp) {
+function getFormattedDueTime(timestamp) {
 
   // format time to show hour and minute only, with leading zeroes
   const timeFormattingOptions = {
@@ -318,24 +298,26 @@ function getDueTime(timestamp) {
   return dueTime;
 }
 
-
-function displayAllTasks() {
-
-  // display all notes
-  const taskList = getTaskListFromLocalStorage();
-
-  taskList.forEach(
-    (task) => {
-      addTaskToDOM(task);
-    }
-  );
-}
-
 function setMinimumDateForToday() {
-
+  const currentTime = new Date();
+  // get the date portion of the ISO string (start - "T")
+  const minimumDate = currentTime.toISOString().split("T")[0];
   // set min date in input
-  document.getElementById("dueDate").setAttribute("min", new Date().toISOString().slice(0, 10));
+  document.getElementById("dueDate").setAttribute("min", minimumDate);
 }
+
+function refocusOnTaskDescriptionInputField(event) {
+  // we only want the focus to happen if the user is not currently focused on another input field
+  if (!event.target.matches(".form-control")) {
+    // user is not focused on another input field
+    document.getElementById("taskDescription").focus();
+  }
+}
+
+
+
+// event listeners
 
 document.addEventListener("DOMContentLoaded", displayAllTasks);
-// document.addEventListener("DOMContentLoaded", setMinimumDateForToday);
+document.addEventListener("DOMContentLoaded", setMinimumDateForToday);
+document.addEventListener("keydown", refocusOnTaskDescriptionInputField);
